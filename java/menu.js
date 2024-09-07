d84842907b0421ec6f6fb4a575fa605a2816ba9e
gut1nh0 <augusto.ferrari.senai404@gmail.com>
2024-09-06 21:15:23 -0300

    responsive

---
+++ java/menu.js
@@ -0,0 +1,10 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.cabecalho_mobile');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "images/ menu.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "images/icon-close-512.webp";
+    }
+}
