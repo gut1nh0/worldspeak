2a773581e4449ffb414c5310836166d8f8f04a2d
gut1nh0 <augusto.ferrari.senai404@gmail.com>
2024-09-07 22:49:49 -0300

    menu-mobile

--- java/menu.js
+++ java/menu.js
@@ -1,8 +1,8 @@
 function menuShow() {
-    let menuMobile = document.querySelector('.cabecalho_mobile');
+    let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "images/ menu.png";
+        document.querySelector('.icon').src = "images/menu.png";
     } else {
         menuMobile.classList.add('open');
         document.querySelector('.icon').src = "images/icon-close-512.webp";
